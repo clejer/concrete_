0df99ee28be001d1d3bc8abecacc3dbf00697677
Jerome <jer.clement@laposte.net>
2013-01-27 18:32:55 +0100

    Install

---
+++ config/site.php
@@ -0,0 +1,6 @@
+<?php 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'concrete5');
+define('PASSWORD_SALT', 'D6YEC1foZrqN44YeXAYVCvKLRhnALsSoNSiFSJUDlM9NYiK0w9OaUHbarNpzXYjt');
